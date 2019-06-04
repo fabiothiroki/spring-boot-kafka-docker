a2cc95986a5a1f381716e553a0ccf6f4e985751a
Fabio Hiroki <fabiothiroki@gmail.com>
2019-06-04 09:41:41 -0300

    Add docker-compose and kafka integration

--- src/main/java/com/example/demo/KafkaController.java
+++ src/main/java/com/example/demo/KafkaController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KafkaController {
 
+    @Autowired
+    private final KafkaProducer producer;
+
+    public KafkaController(final KafkaProducer producer) {
+        this.producer = producer;
+    }
+
     @RequestMapping("/publish")
     public String sendMessageToKafkaTopic(@RequestParam("message") final String message){
+        this.producer.sendMessage(message);
         return "Hello World!";
     }
 
